2a8d9583d2351f791761e8d8c2804577adb5564f
Tomasz Leś <szkolenia@tomaszles.pl>
2023-05-16 14:29:51 +0200

    Add project files.

---
+++ P02SasSExample/SpeechServiceTool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02SasSExample
+{
+    class SpeechServiceTool
+    {
+        //dotnet add package Microsoft.CognitiveServices.Speech
+        private async Task<string> RecognizeFromMic(SpeechConfig speechConfig)
+        {
+            using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using var recognizer = new SpeechRecognizer(speechConfig, "pl-PL", audioConfig);
+
+            var result = await recognizer.RecognizeOnceAsync();
+            return result.Text;
+        }
+    }
+}
